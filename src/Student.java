a3508677052eba2b8bf58f87c652f61b699feb76
James <james526375@outlook.com>
2026-03-06 10:07:25 +0800

    feat: add variables to Person and Student classes

--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,11 @@
+import java.util.ArrayList;
+
 public class Student extends Person {
-    
+    public int studentId;
+    public double gpa;
+    public String semester;
+    public ArrayList<String> subjects = new ArrayList<>();
+    public ArrayList<Integer> grades = new ArrayList<>();
+    public ArrayList<String> gradeLevel = new ArrayList<>();
     
 }
